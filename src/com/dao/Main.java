8785d59f4de14ee65c00e48cef156a3bbc310f84
prathmeshgirulkar <prathmeshgirulkar11@gmail.com>
2025-11-05 09:51:31 +0530

    first version

---
+++ src/com/dao/Main.java
@@ -0,0 +1,32 @@
+package com.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.entity.Student;
+
+public class Main {
+	public static void main(String[] args) {
+		Configuration cf = new Configuration();
+		cf.configure();
+
+		cf.addAnnotatedClass(Student.class);
+		SessionFactory bs = cf.buildSessionFactory();
+		Session os = bs.openSession();// session ha 1 session ahe yechat apan karnar ahe zalyvr close karacha
+		Transaction bt = os.beginTransaction();// update delete ani insert sathi lagte, ani kam zalyvrr commmit karacha
+		Student s1 = new Student();
+
+		s1.setId(2);
+		s1.setName("rahukl");
+		s1.setAddress("mumbai");
+//	   os.save(s);
+
+		os.update(s1);
+		/// os.get(1, id);
+		bt.commit();
+		os.close();
+
+	}
+}
---
+++ src/com/entity/Student.java
@@ -0,0 +1,39 @@
+package com.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+//@Table(name = "") it is use to change the table name
+public class Student {
+	@Id
+	//@Column(name="id")
+	private int id;
+	private String name;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+	
+
+}
